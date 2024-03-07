ecfab41a3a0a01e6bdcc64722dfadf115250e270
musharaf78 <162351009+musharaf78@users.noreply.github.com>
2024-03-07 07:13:31 +0000

    conversion of data types

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "musharaf"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; fale => 0
+
+isLoggedIn = "musharaf"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"mushraf" => true
+
+let SomeNumber = 33
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
