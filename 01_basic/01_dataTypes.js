526e9d0a87b4ab35ec38f7783d7a0db0326fb55f
musharaf78 <162351009+musharaf78@users.noreply.github.com>
2024-03-07 04:38:13 +0000

    datatypes

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";// treat all js code newer version
+
+// aleart(3+3)// we are using node js, not browser
+
+console.log(3
+    +
+    3)// code readability should be high
+
+console.log("musharaf")
+
+
+let name = "mushraf"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null  => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
